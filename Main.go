481187876fef4b8f8c8e1dbe3982910b7f93bef9
Dominic Lehrmoser <dlehrmos@stud.hs-heilbronn.de>
2017-11-13 18:39:33 +0100

    finished crawler

--- Main.go
+++ Main.go
@@ -5,6 +5,9 @@ import (
 	"net/http"
 	"io/ioutil"
 	"strings"
+	"log"
+	"os"
+	"io"
 )
 var accounts []string
 
@@ -15,6 +18,7 @@ func main() {
 	for _, element := range accounts {
 		crawl(element)
 	}
+	fmt.Println("Crawler finished")
 }
 
 func crawl(url string)  {
@@ -32,13 +36,52 @@ func crawl(url string)  {
 	chunks := strings.Split(str,",")
 	for _, element := range chunks {
 		if(strings.HasPrefix(element," " + `"` + "display_src")) {
-			url := strings.TrimLeft(element," " + `"` + "display_src: " + `"`)
-			url = strings.TrimRight(url,`"`)
-			fmt.Println(url)
+			pic := strings.TrimLeft(element," " + `"` + "display_src: " + `"`)
+			pic = strings.TrimRight(pic,`"`)
+			archive(pic,strings.Split(url,"/")[3])
 		}
 	}
 }
 
-func save(url string)  {
+func archive(pictureurl string, username string)  {
+	//fmt.Println("saving " + pictureurl + " from " + username)
+	if !alreadySaved(username + "/" + strings.Split(pictureurl,"/")[len(strings.Split(pictureurl,"/")) - 1]) {
+		save(pictureurl, username)
+	}
+}
 
+func save(pictureurl string, username string)  {
+	fmt.Println("Saving new Image " + pictureurl)
+	response, e := http.Get(pictureurl)
+	if e != nil {
+		log.Fatal(e)
+	}
+	defer response.Body.Close()
+
+	//create directory
+	if _, err := os.Stat(username); os.IsNotExist(err) {
+		err = os.MkdirAll(username, 0777)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	//open a file for writing
+	file, err := os.Create(username + "/" + strings.Split(pictureurl,"/")[len(strings.Split(pictureurl,"/")) - 1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = io.Copy(file, response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	file.Close()
+}
+
+func alreadySaved(fullpath string)(exists bool)  {
+	if _, err := os.Stat(fullpath); os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
