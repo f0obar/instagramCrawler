c2e635ff18a642fec650731954b6c4ef7c1e51f9
Dominic Lehrmoser <dlehrmos@stud.hs-heilbronn.de>
2017-11-13 19:43:42 +0100

    crawler parses interval time from config and repeats itself automatically

--- Main.go
+++ Main.go
@@ -9,31 +9,55 @@ import (
 	"os"
 	"io"
 	"strconv"
+	"time"
 )
 
 func main() {
-
 	fmt.Println("Crawler starting")
 
-	for _, element := range getConfig() {
-		crawl(element)
+	config := getConfig()
+	interval := -1
+
+	if num, err := strconv.Atoi(config[0]); err == nil {
+		interval = num
+		fmt.Println("Setting repeat interval to " + config[0] + " seconds")
+		config = append(config[:0], config[1:]...)
 	}
-	fmt.Println("Crawler finished")
+	fmt.Println("Found " + strconv.Itoa(len(config)) + " Accounts to crawl")
+
+
+	if interval > 0 {
+		t := time.NewTicker(time.Duration(interval)*time.Second)
+		for {
+			fmt.Println(">>CRAWLING<<")
+			for _, element := range config {
+				crawl(element)
+			}
+			fmt.Println(">>CRAWLING FINISHED, next crawling will start in " + strconv.Itoa(interval) + " seconds<<")
+			<-t.C
+		}
+	} else {
+		for _, element := range config {
+			crawl(element)
+		}
+		fmt.Println("Crawler finished")
+	}
 }
 
 func getConfig()(accounts []string)  {
 	b, err := ioutil.ReadFile("config.txt")
 	if err != nil {
 		fmt.Print("Couldn't read config.txt")
+		panic(err)
 	}
 	for _, element := range strings.Split(string(b),",") {
 		accounts = append(accounts, element)
 	}
-	fmt.Println("Found " + strconv.Itoa(len(accounts)) + " Accounts to crawl")
 	return accounts
 }
 
 func crawl(url string)  {
+	fmt.Println("Crawling: " + url)
 	resp, err := http.Get(url)
 	defer resp.Body.Close()
 
