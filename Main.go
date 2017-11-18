b92be255b617e0011799ac7522b3505d97bc7b4d
Dominic Lehrmoser <dlehrmos@stud.hs-heilbronn.de>
2017-11-18 01:03:58 +0100

    complete rework with correct structs for html, worker pool and timestamp

--- Main.go
+++ Main.go
@@ -9,221 +9,271 @@ import (
 	"os"
 	"strconv"
 	"time"
-	"sync"
 	"io"
 	"regexp"
+	"sync"
+	"github.com/anaskhan96/soup"
+	"encoding/json"
 )
-var waitGroup sync.WaitGroup
-var mutex sync.Mutex
 
-var allAccounts []string
-var madness = false
+var waitGorup = sync.WaitGroup{}
+var workers = 5
+var pages = -1
+var interval = -1
+var accountsToCrawl chan string
 
 func main() {
 	fmt.Println("Crawler starting")
+	params := append(os.Args[:0], os.Args[1:]...)
 
-	config := getConfig()
-	interval := -1
-	pagesToCrawlBack := 0
-
-	if num, err := strconv.Atoi(config[0]); err == nil {
-		interval = num
-		fmt.Println("Setting repeat interval to " + config[0] + " seconds")
-		config = append(config[:0], config[1:]...)
-	} else if strings.HasPrefix(config[0],"P") {
-		pagesToCrawlBack, err = strconv.Atoi(strings.TrimLeft(config[0],"P"))
-		if(err != nil){
-			panic(err)
+	for _, element := range params {
+		if strings.HasPrefix(element, "w") {
+			reg, err := regexp.Compile("[^0-9]+")
+			if err != nil {
+				log.Fatal(err)
+			}
+			processedString := reg.ReplaceAllString(element, "")
+			if num, err := strconv.Atoi(processedString); err == nil {
+				workers = num
+			}
 		}
-		if pagesToCrawlBack == -2 {
-			madness = true
+		if strings.HasPrefix(element, "i") {
+			reg, err := regexp.Compile("[^0-9]+")
+			if err != nil {
+				log.Fatal(err)
+			}
+			processedString := reg.ReplaceAllString(element, "")
+			if num, err := strconv.Atoi(processedString); err == nil {
+				interval = num
+			}
 		}
-		fmt.Println("Crawlback mode: " + config[0] + " Pages")
-		config = append(config[:0], config[1:]...)
+		if strings.HasPrefix(element, "p") {
+			reg, err := regexp.Compile("[^0-9]+")
+			if err != nil {
+				log.Fatal(err)
+			}
+			processedString := reg.ReplaceAllString(element, "")
+			if num, err := strconv.Atoi(processedString); err == nil {
+				pages = num
+			}
+		}
 	}
-	fmt.Println("Found " + strconv.Itoa(len(config)) + " Accounts to crawl")
-	allAccounts = config
+
+	fmt.Println("Workerpool Size:", workers)
+	fmt.Println("Pages to Crawl per profile:", pages)
+	fmt.Println("Refreshing interval:", interval, "seconds")
 
 	if interval > 0 {
-		t := time.NewTicker(time.Duration(interval)*time.Second)
+		t := time.NewTicker(time.Duration(interval) * time.Second)
 		for {
-			fmt.Println(">>CRAWLING<<")
-			for _, element := range config {
-				go crawl(element,pagesToCrawlBack)
-			}
-			waitGroup.Wait()
-			fmt.Println(">>CRAWLING FINISHED, next crawling will start in " + strconv.Itoa(interval) + " seconds<<")
+			fmt.Println(">>CRAWLING<<\n\n\n\n")
+			startCrawling()
+			fmt.Println(">>CRAWLING FINISHED, next crawling will start in " + strconv.Itoa(interval) + " seconds\n\n\n\n<<")
 			<-t.C
 		}
 	} else {
-		for _, element := range config {
-			go crawl(element,pagesToCrawlBack)
-		}
-		time.Sleep(100)
-		waitGroup.Wait()
+		fmt.Println(">>CRAWLING<<\n\n\n\n")
+		startCrawling()
 		fmt.Println("Crawler finished")
 	}
 }
 
-func getConfig()(accounts []string)  {
-	b, err := ioutil.ReadFile("config.txt")
+func startCrawling() {
+	accountsToCrawl = make(chan string, 1000)
+
+	readAccountsFile()
+	for w := 1; w <= workers; w++ {
+		go workerRun(w)
+	}
+	time.Sleep(100)
+	waitGorup.Wait()
+}
+
+func readAccountsFile()  {
+	b, err := ioutil.ReadFile("accounts.txt")
 	if err != nil {
-		fmt.Print("Couldn't read config.txt")
+		fmt.Print("Couldn't read accounts.txt")
 		panic(err)
 	}
 	for _, element := range strings.Split(string(b),",") {
-		accounts = append(accounts, element)
+		addAccount(element)
 	}
-	return accounts
 }
 
-func crawl(url string, pages int)  {
-	waitGroup.Add(1)
-	defer waitGroup.Done()
+func workerRun(i int) {
+	for account := range accountsToCrawl {
+		fmt.Println("Worker",i,"crawling: ",account)
+		crawl(account)
+		waitGorup.Done()
+	}
+}
 
-	//create directory
-	if _, err := os.Stat(strings.Split(url,"/")[3]); os.IsNotExist(err) {
-		err = os.MkdirAll(strings.Split(url,"/")[3], 0777)
+func addAccount(acc string)  {
+	waitGorup.Add(1)
+	accountsToCrawl <- acc
+}
+
+func crawl(account string)  {
+	fmt.Println("+++crawling:",account,"+++")
+
+	//Checking / Creating Foler for the account
+	if _, err := os.Stat(account); os.IsNotExist(err) {
+		err = os.MkdirAll(account, 0777)
 		if err != nil {
 			panic(err)
 		}
 	}
+	baseUrl:= "https://www.instagram.com/" + account + "/"
 
-
-	resp, err := http.Get(url)
-	defer resp.Body.Close()
-
-	if err != nil {
-		panic(err)
-	}
-	html, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	str := fmt.Sprintf("%s", html)
-	chunks := strings.Split(str,",")
-
-	nextPageId := ""
-	crawlSubImages := false
-	idCount := 0
-
-
-	for _, element := range chunks {
-		/*
-		When media preview is null the image actually is a gallery
-		 */
-		if strings.HasPrefix(element," " + `"` + "media_preview") {
-			mediaType := strings.TrimLeft(element," " + `"` + "media_preview" + `"` + ": ")
-			if mediaType == "null"{
-				crawlSubImages = true
+	if pages == -1 {
+		//crawl full page
+		nextID := openPage(baseUrl, account)
+		for nextID != "" {
+			nextID = openPage(baseUrl + "?max_id=" + nextID, account)
+		}
+	} else {
+		if pages > 0 {
+			pagesLeft := pages - 1
+			nextID := openPage(baseUrl, account)
+			for nextID != "" && pagesLeft > 0{
+				nextID = openPage(baseUrl + "?max_id=" + nextID, account)
+				pagesLeft--
 			}
 		}
-		/*
-		Code tag for gallery id to crawl containing images
-		 */
-		if strings.HasPrefix(element," " + `"` + "code") && crawlSubImages {
-			crawlSubImages = false
-			id := strings.TrimLeft(element," " + `"` + "code" + `"` + ": " + `"`)
-			id = strings.TrimRight(id,`"`)
-			go crawlSub(id,strings.Split(url,"/")[3])
-		}
-		/*
-		Simple image, save directly.
-		 */
-		if strings.HasPrefix(element," " + `"` + "display_src") && crawlSubImages == false{
-			pic := strings.TrimLeft(element," " + `"` + "display_src: " + `"`)
-			pic = strings.TrimRight(pic,`"`)
-			go archive(pic,strings.Split(url,"/")[3])
-		}
-		/*
-		Tag for crawling older page
-		 */
-		if strings.HasPrefix(element," " + `"` + "id"){
-			nextPageId = strings.TrimLeft(element," " + `"` + "id" + `"` + ": " + `"`)
-			nextPageId = strings.TrimRight(nextPageId,`"`)
-			idCount++
-		}
-		/**
-		madness mode
-		 */
-		 if madness {
-			 if strings.HasPrefix(element," " + `"` + "caption") && strings.Contains(element,"@"){
-				 description := strings.TrimLeft(element," " + `"` + "caption" + `"` + ": ")
-				 description = strings.TrimRight(description,`"`)
-				 possibleAccs := strings.Split(description, " ")
-
-				 for _, acc := range possibleAccs {
-				 	if strings.HasPrefix(acc,"@"){
-						reg, err := regexp.Compile("[^a-zA-Z0-9_]+")
-						if err != nil {
-							log.Fatal(err)
-						}
-						processedString := reg.ReplaceAllString(acc, "")
-						if len(processedString) > 0 {
-							go addNewlyCralwedAccount(strings.TrimLeft(processedString, "@"))
-						}
-					}
-				 }
-			 }
-		 }
 	}
-	if(pages != 0 && idCount == 13){
-		if !strings.HasSuffix(url,"/"){
-			//need to trim max_id off
-			url = strings.TrimRight(url,strings.Split(url,"/")[len(strings.Split(url,"/")) -1])
-		}
+	fmt.Println("---crawling finished:",account,"---")
+}
 
-		url+= "?max_id="
-		url+= nextPageId
-
-		pages--
-		crawl(url,pages)
-	}
+type MainPage struct {
+	EntryData struct{
+		ProfilePage []struct{
+			User struct{
+				Media struct{
+					Nodes []struct{
+						Typename string `json:"__typename"`
+						Id string `json:"id"`
+						MediaPreview string `json:"media_preview"`
+						IsVideo bool `json:"is_video"`
+						Code string `json:"code"`
+						Date int `json:"date"`
+						DisplaySrc string `json:"display_src"`
+						Caption string `json:"caption"`
+					} `json:"nodes"`
+					PageInfo struct{
+						HasNextPage bool `json:"has_next_page"`
+					} `json:"page_info"`
+				} `json:"media"`
+			} `json:"user"`
+		} `json:"ProfilePage"`
+	} `json:"entry_data"`
 }
 
 
-func crawlSub(id string, username string){
-	waitGroup.Add(1)
-	defer waitGroup.Done()
-	resp, err := http.Get("https://www.instagram.com/p/" + id)
-	defer resp.Body.Close()
-
+func openPage(url string, accountname string)(nextPageID string){
+	resp, err := soup.Get(url)
 	if err != nil {
 		panic(err)
 	}
-	html, err := ioutil.ReadAll(resp.Body)
+	doc := soup.HTMLParse(resp)
+	script := doc.FindAll("script")[2].Text()
+	script = script[21:len(script)-1]
+
+	var data map[string]interface{}
+	e := json.Unmarshal([]byte(script), &data)
+	if e != nil {
+		panic(e)
+	}
+
+	page := MainPage{}
+	json.Unmarshal([]byte(script), &page)
+
+
+	for _, element := range page.EntryData.ProfilePage[0].User.Media.Nodes {
+		if !element.IsVideo{
+			if element.Typename == "GraphImage" {
+				waitGorup.Add(1)
+				go archive(element.DisplaySrc,accountname,element.Date)
+			}
+			if element.Typename == "GraphSidecar" {
+				waitGorup.Add(1)
+			 	go openGallery("https://www.instagram.com/p/" + element.Code,accountname,element.Date)
+			}
+		}
+	}
+
+	if page.EntryData.ProfilePage[0].User.Media.PageInfo.HasNextPage {
+		return page.EntryData.ProfilePage[0].User.Media.Nodes[11].Id
+	}
+	return ""
+}
+
+
+type GalleryPage struct {
+	EntryData struct {
+		PostPage []struct {
+			Graphql struct {
+				ShortcodeMedia struct {
+					EdgeSidecarToChildren struct {
+						Edges []struct {
+							Node struct {
+								Typename     string `json:"__typename"`
+								Id           string `json:"id"`
+								MediaPreview string `json:"media_preview"`
+								IsVideo      bool   `json:"is_video"`
+								DisplaySrc   string `json:"display_url"`
+							} `json:"node"`
+						} `json:"edges"`
+					} `json:"edge_sidecar_to_children"`
+				} `json:"shortcode_media"`
+			} `json:"graphql"`
+		} `json:"PostPage"`
+	} `json:"entry_data"`
+}
+
+func openGallery(url string, accountname string,timestamp int)  {
+	resp, err := soup.Get(url)
 	if err != nil {
 		panic(err)
 	}
+	doc := soup.HTMLParse(resp)
+	script := doc.FindAll("script")[2].Text()
+	script = script[21:len(script)-1]
 
-	str := fmt.Sprintf("%s", html)
-	chunks := strings.Split(str,",")
-	for _, element := range chunks {
-		if strings.HasPrefix(element," " + `"` + "display_url") {
-			pic := strings.TrimLeft(element," " + `"` + "display_url: " + `"`)
-			pic = strings.TrimRight(pic,`"`)
-			go archive(pic,username)
+	var data map[string]interface{}
+	e := json.Unmarshal([]byte(script), &data)
+	if e != nil {
+		panic(e)
+	}
+
+	page := GalleryPage{}
+	json.Unmarshal([]byte(script), &page)
+
+	for _, element := range page.EntryData.PostPage[0].Graphql.ShortcodeMedia.EdgeSidecarToChildren.Edges {
+		if element.Node.Typename == "GraphImage" {
+			waitGorup.Add(1)
+			go archive(element.Node.DisplaySrc,accountname,timestamp)
 		}
 	}
+	waitGorup.Done()
 }
 
+func archive(pictureurl string, username string,timestamp int)  {
+	fullpath := username + "/" + strconv.Itoa(timestamp) + "_" +strings.Split(pictureurl,"/")[len(strings.Split(pictureurl,"/")) - 1]
 
-func archive(pictureurl string, username string)  {
-	waitGroup.Add(1)
-	defer waitGroup.Done()
-	if !alreadySaved(username + "/" + strings.Split(pictureurl,"/")[len(strings.Split(pictureurl,"/")) - 1]) {
-		save(pictureurl, username)
+	if _, err := os.Stat(fullpath); os.IsNotExist(err) {
+		save(pictureurl, fullpath)
 	}
+	waitGorup.Done()
 }
 
-func save(pictureurl string, username string) {
+func save(pictureurl string, fullpath string) {
 	response, e := http.Get(pictureurl)
 	if e != nil {
 		log.Fatal(e)
 	}
 	defer response.Body.Close()
 
-	file, err := os.Create(username + "/" + strings.Split(pictureurl, "/")[len(strings.Split(pictureurl, "/"))-1])
+	file, err := os.Create(fullpath)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -233,38 +283,4 @@ func save(pictureurl string, username string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func alreadySaved(fullpath string)(exists bool)  {
-	if _, err := os.Stat(fullpath); os.IsNotExist(err) {
-		return false
-	}
-	return true
-}
-
-func addNewlyCralwedAccount(acc string)  {
-	waitGroup.Add(1)
-	mutex.Lock()
-	if !contains(allAccounts,acc){
-		fmt.Println("New Account Found:",acc)
-		//Write it to file
-		f, _ := os.OpenFile("config.txt", os.O_APPEND|os.O_WRONLY, 0777)
-		f.WriteString(",https://www.instagram.com/"+acc+"/")
-		f.Close()
-		//add it to array
-		allAccounts = append(allAccounts,acc)
-		//start new routine
-		//go crawl("",-2)
-	}
-	mutex.Unlock()
-	waitGroup.Done()
-}
-
-func contains(s []string, e string) bool {
-	for _, a := range s {
-		if a == e {
-			return true
-		}
-	}
-	return false
 }
